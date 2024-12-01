249a732a43ed6345ebfb956e90b4468f2ad46bfe
ARQAM 🇵🇸 <107029929+qsud@users.noreply.github.com>
2024-12-01 18:32:35 +0530

    Update m.py

--- m.py
+++ m.py
@@ -5,6 +5,7 @@ import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import threading
 import time
+import logging
 
 logging.basicConfig(filename="bot_errors.log", level=logging.ERROR, format="%(asctime)s - %(levelname)s - %(message)s")
 
